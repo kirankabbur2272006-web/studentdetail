d26d021b49117947978f346ea442f2981b4921b5
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-11-27 14:17:13 +0530

    Update std.py

--- std.py
+++ std.py
@@ -1,20 +1,19 @@
 import sys
 
+# Check if 5 command-line values were provided
 if len(sys.argv) == 6:
-    script_name = sys.argv[0]
-    n1 = int(sys.argv[1])
-    n2 = int(sys.argv[2])
-    n3 = int(sys.argv[3])
-    n4 = int(sys.argv[4])
-    n5 = int(sys.argv[5])
+    try:
+        n1 = int(sys.argv[1])
+        n2 = int(sys.argv[2])
+        n3 = int(sys.argv[3])
+        n4 = int(sys.argv[4])
+        n5 = int(sys.argv[5])
+    except ValueError:
+        print("ERROR: All inputs must be integers.")
+        sys.exit(1)
 else:
     print("NO input values - Using default values")
-    script_name = sys.argv[0]
-    n1 = 60
-    n2 = 80
-    n3 = 75
-    n4 = 60
-    n5 = 88
+    n1, n2, n3, n4, n5 = 60, 80, 75, 60, 88
 
 avg = (n1 + n2 + n3 + n4 + n5) / 5
 print(f"Average is: {avg}")
@@ -30,4 +29,4 @@ elif avg > 60:
 elif avg > 50:
     print("E grade")
 else:
-    print("FAIL")
+    print("FAIL")
