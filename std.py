e54fd9003718c22e4218c6c0dd282a4429d89cc4
kirankabbur2272006-web <kirankabbur2272006@gmail.com>
2025-11-27 14:02:05 +0530

    Create std.py

---
+++ std.py
@@ -0,0 +1,33 @@
+import sys
+if len(sys.argv)!=6:
+  script_name = sys.argv[0]
+  n1=sys.argv[1]
+  n2=sys.argv[2]
+  n3=sys.argv[3]
+  n4=sys.argv[4]
+  n5=sys.argv[5]
+
+else:
+  print("NO input values - Using default values")
+  script_name=sys.argv[0]
+  n1=60
+  n2=80
+  n3=75
+  n4=60
+  n5=88
+
+avg=(n1+n2+n3+n4+n5)
+print(f"Average is:{avg}")
+
+if avg>90:
+  print("A grade")
+elif avg>80:
+  print("B grade")
+elif avg>70:
+  print("C grade")
+elif avg>60:
+  print("D grade")
+elif avg>50:
+  print("E grade")
+else 
+print("FAIL")
